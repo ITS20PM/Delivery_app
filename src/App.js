ed8221a0cae2a416399df7e15719c502bc6cea78
ITS20PM <patrick21meng@gmail.com>
2023-10-26 12:35:36 -0700

    add weather information

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import UilReact from '@iconscout/react-unicons/icons/uil-react';
 import Buttons from './Components/Buttons';
 import Inputs from './Components/Inputs';
+import Time_Location from './Components/Time_Location';
 
 function App() {
   return <div className='mx-auto max-w-screen-md mt-4 py-5 px-32 bg-gradient-to-br from-cyan-700 to-blue-700 h-fit shadow-xl 
@@ -9,6 +10,7 @@ function App() {
 
     <Buttons />
     <Inputs />
+    <Time_Location />
 
   </div>;
 }
--- src/Components/Inputs.jsx
+++ src/Components/Inputs.jsx
@@ -9,8 +9,16 @@ function Inputs() {
             placeholder='search for city...'
             className='text-xl font-light p2 w-full shadow-xl focus:outline-none capitalize placeholder:lowercase'/>
         
-            <UilSearch size={25} className="text-white cursor-pointer"></UilSearch>
-            <UilLocationPoint size={25} className="text-white cursor-pointer"></UilLocationPoint>
+            <UilSearch size={25} className="text-white cursor-pointer transition ease-out hover:scale-125"></UilSearch>
+            <UilLocationPoint size={25} className="text-white cursor-pointer transition ease-out hover:scale-125"></UilLocationPoint>
+        </div>
+
+        <div className='flex flex-row w-1/4 items-center justify-center'>
+          <button name="metric" className='text-xl text-white font-light'>°C</button>
+
+          <p className='text-xl text-white mx-1'>|</p>
+
+          <button name="imperial" className='text-xl text-white font-light'>°F</button>
         </div>
     </div>
   )
