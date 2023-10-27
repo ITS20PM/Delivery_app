09842f4c2acc2df74345a4247527b31a4affe6c7
ITS20PM <patrick21meng@gmail.com>
2023-10-27 01:30:59 -0700

    working on weather service

--- src/App.js
+++ src/App.js
@@ -3,16 +3,32 @@ import UilReact from '@iconscout/react-unicons/icons/uil-react';
 import Buttons from './Components/Buttons';
 import Inputs from './Components/Inputs';
 import Time_Location from './Components/Time_Location';
+import Temperature_Detail from './Components/Temperature_Detail';
+import Forecast from './Components/Forecast';
+import weather_data from './Services/weatherServices';
 
 function App() {
-  return <div className='mx-auto max-w-screen-md mt-4 py-5 px-32 bg-gradient-to-br from-cyan-700 to-blue-700 h-fit shadow-xl 
-  shadow-gray-400'>
 
-    <Buttons />
-    <Inputs />
-    <Time_Location />
+  const fetch_weather = async () => {
+    const data = await get_formatted_weather_data('weather', {q: "london"});
+    console.log(data);
+  };
 
-  </div>;
+  return (
+    <div className='mx-auto max-w-screen-md mt-4 py-5 px-32 bg-gradient-to-br from-cyan-700 to-blue-700 h-fit shadow-xl 
+    shadow-gray-400'>
+
+      <Buttons />
+      <Inputs />
+      
+      <Time_Location />
+      <Temperature_Detail />
+      
+      <Forecast title="hourly forecast"/>
+      <Forecast title="daily forecast"/>
+
+    </div>
+  );
 }
 
 export default App;
---
+++ src/Services/weatherServices.js
@@ -0,0 +1,3 @@
+const API_Key = "88a3affac7dc381106ad2e97e581b508";
+const base_url = "https://api.openweathermap.org/data/2.5/";
+
