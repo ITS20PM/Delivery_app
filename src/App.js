fcd78fdbddf7b100c92f5a0f6a5505ee9c332144
ITS20PM <patrick21meng@gmail.com>
2023-10-27 13:15:39 -0700

    needs to fix typeerror in slice()

--- src/App.js
+++ src/App.js
@@ -6,13 +6,21 @@ import Time_Location from './Components/Time_Location';
 import Temperature_Detail from './Components/Temperature_Detail';
 import Forecast from './Components/Forecast';
 import weather_data from './Services/weatherServices';
+import get_formatted_weather_data from './Services/weatherServices';
+import { useEffect, useState } from 'react';
 
 function App() {
 
+  const [query, setQuery] = useState({q: "berlin"});
+  const [units, setUnits] = useState('metric');
+  const [weather, setWeather] = useState(null);
+
   const fetch_weather = async () => {
-    const data = await get_formatted_weather_data('weather', {q: "london"});
+    const data = await get_formatted_weather_data({q: "london"});
     console.log(data);
   };
+
+  fetch_weather();
 
   return (
     <div className='mx-auto max-w-screen-md mt-4 py-5 px-32 bg-gradient-to-br from-cyan-700 to-blue-700 h-fit shadow-xl 
